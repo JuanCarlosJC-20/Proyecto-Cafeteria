03915d559ed1ce4da7e18f9b462394a80ebb5ed7
Juancarlos <sampfz23@gmail.com>
2026-02-20 07:34:42 +0100

    controller y logica de service

--- cafeteria-crud/src/main/java/com/sena/cafeteria_crud/controller/PersonController.java
+++ cafeteria-crud/src/main/java/com/sena/cafeteria_crud/controller/PersonController.java
@@ -1,5 +1,51 @@
 package com.sena.cafeteria_crud.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sena.cafeteria_crud.dto.Request.personDto;
+import com.sena.cafeteria_crud.service.Impl.PersonService;
+
+
+
+@RestController
+@RequestMapping("/api/person")
 public class PersonController {
 
+    @Autowired
+    private PersonService personService;
+
+
+    @PostMapping 
+    public String  registerPerson(@Validated @RequestBody personDto personDto)
+     {
+        personService.savaPerson(personDto);
+        return "Persona registrada exitosamente";
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> getPersonAll()
+    {
+        var obtiene = personService.getAllPersons();
+        return new ResponseEntity<>(obtiene, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deletePerson(@PathVariable Long id){
+        personService.deletepito(id);
+        return true;
+    }
+
+   
+    
 }
--- cafeteria-crud/src/main/java/com/sena/cafeteria_crud/service/Impl/PersonService.java
+++ cafeteria-crud/src/main/java/com/sena/cafeteria_crud/service/Impl/PersonService.java
@@ -1,8 +1,52 @@
 package com.sena.cafeteria_crud.service.Impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.sena.cafeteria_crud.dto.Request.personDto;
+import com.sena.cafeteria_crud.model.Person;
+import com.sena.cafeteria_crud.repository.IPersonRepository;
 
 @Service
 public class PersonService {
 
+    @Autowired
+    private IPersonRepository IpersonRepository;
+
+    // Método para obtener todas las personas
+
+    public List<personDto> getAllPersons() {
+        // Lógica para obtener todas las personas
+        return IpersonRepository.findAll().stream()
+                .map(person -> new personDto(
+                        person.getFirstName(),
+                        person.getLastName(),
+                        person.getEmail(),
+                        person.getPhone()))
+                .toList();
+    }
+
+
+    // Método para guardar una persona
+
+    public boolean savaPerson(personDto personDto) {
+        // Lógica para guardar una persona
+        var person = new Person();
+        person.setFirstName(personDto.getFirstName());
+        person.setLastName(personDto.getLastName());
+        person.setEmail(personDto.getEmail());
+        person.setPhone(personDto.getPhone());
+        IpersonRepository.save(person);
+        return true;
+    }
+
+    public boolean deletepito(Long id){
+        IpersonRepository.deleteById(id);
+        return true;
+    }
+
+
+       
 }
